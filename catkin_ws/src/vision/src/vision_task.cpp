474323e33e32c6a96fef6b631350430e800c27af
Sergii Penner <sergmister1@gmail.com>
2021-09-26 15:41:15 -0600

    initial commit

--- catkin_ws/src/vision/src/vision_task.cpp
+++ catkin_ws/src/vision/src/vision_task.cpp
@@ -5,7 +5,6 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
-
 class ImageConverter
 {
     ros::NodeHandle nh_;
@@ -16,29 +15,26 @@ class ImageConverter
 
 public:
     ImageConverter()
-            : it_(nh_)
+        : it_(nh_)
     {
         // Subscribe to input video feed and publish output video feed
         image_sub_ = it_.subscribe("/jetbot_camera/image_raw", 1,
                                    &ImageConverter::imageCb, this);
         image_pub_ = it_.advertise("/image_converter/output_video", 1);
-
-
     }
 
     ~ImageConverter()
     {
-
     }
 
-    void imageCb(const sensor_msgs::ImageConstPtr& msg)
+    void imageCb(const sensor_msgs::ImageConstPtr &msg)
     {
         cv_bridge::CvImagePtr cv_ptr;
         try
         {
             cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
         }
-        catch (cv_bridge::Exception& e)
+        catch (cv_bridge::Exception &e)
         {
             ROS_ERROR("cv_bridge exception: %s", e.what());
             return;
@@ -48,17 +44,16 @@ public:
 
         // Draw an example circle on the video stream
         if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-            cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+            cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255, 0, 0));
 
         // YOUR CODE ABOVE
-
 
         // Output modified video stream
         image_pub_.publish(cv_ptr->toImageMsg());
     }
 };
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 {
     ros::init(argc, argv, "image_converter");
     ImageConverter ic;
