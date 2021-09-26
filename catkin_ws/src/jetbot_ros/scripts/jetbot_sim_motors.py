203213e4432f303c4df49f98b56522078ad51b6a
Justin <jkf@ualberta.ca>
2021-09-26 05:54:33 -0600

    fixed motor script

--- catkin_ws/src/jetbot_ros/scripts/jetbot_sim_motors.py
+++ catkin_ws/src/jetbot_ros/scripts/jetbot_sim_motors.py
@@ -4,33 +4,34 @@ from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
 pub = rospy.Publisher('diffdrive/cmd_vel', Twist)
+twist = Twist()
 
 # simple string commands (left/right/forward/backward/stop)
 def on_cmd_str(msg):
 	if msg.data.lower() == "right":
-		pub.linear.x = 0.1
-		pub.linear.y = 0.0
-		pub.linear.z = 0.0
-		pub.angular.y = 0.0
-		pub.angular.y = 0.0
-		pub.angular.z = 0.0
+		twist.linear.x = 0.0
+		twist.linear.y = 0.0
+		twist.linear.z = 0.0
+		twist.angular.y = 0.0
+		twist.angular.y = 0.0
+		twist.angular.z = 0.05
 	elif msg.data.lower() == "left":
-		pub.linear.x = 0.0
-		pub.linear.y = 0.0
-		pub.linear.z = 0.0
-		pub.angular.y = 0.0
-		pub.angular.y = 0.0
-		pub.angular.z = -0.05
+		twist.linear.x = 0.0
+		twist.linear.y = 0.0
+		twist.linear.z = 0.0
+		twist.angular.y = 0.0
+		twist.angular.y = 0.0
+		twist.angular.z = 0.05
 	elif msg.data.lower() == "forward":
-		pub.linear.x = 0.0
-		pub.linear.y = 0.0
-		pub.linear.z = 0.0
-		pub.angular.y = 0.0
-		pub.angular.y = 0.0
-		pub.angular.z = 0.05
+		twist.linear.x = 0.05
+		twist.linear.y = 0.0
+		twist.linear.z = 0.0
+		twist.angular.y = 0.0
+		twist.angular.y = 0.0
+		twost.angular.z = 0.0
 	else:
 		pass
-	pub.publish()
+	pub.publish(twist)
 
 # initialization
 if __name__ == '__main__':
