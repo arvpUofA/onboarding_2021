3ac4ba98e3936bc25db53b6ed6ceb66c99b36d3d
Justin <jkf@ualberta.ca>
2021-09-26 17:12:19 -0600

    removed old code

--- catkin_ws/src/control/src/control_node.cpp
+++ catkin_ws/src/control/src/control_node.cpp
@@ -6,9 +6,9 @@
 ros::Subscriber box;
 ros::Publisher motor_pub;
 std_msgs::String motorString;
-std_msgs::String stopString;
 
-// Create your callback function that takes in the std_msgs::Int32 message as a ConstPtr, sets motorString.data, and publishes motorString
+// Create your callback function for your publisher that takes in the std_msgs::Int32 message as a ConstPtr,
+// sets motorString.data, and publishes motorString.
 
 
 int main(int argc, char** argv)
@@ -17,10 +17,9 @@ int main(int argc, char** argv)
     ros::NodeHandle nh;
     // Declare the motor publisher here that publishes a std_msgs::String to "/jetbot_motors/cmd_str"
     
-    // Declare a subscriber here that subscribes to a std_msgs::Int32 published on "/x_center"
+    // Declare a subscriber here that subscribes to a std_msgs::Int32 published on your bounding box center publisher
     
     ros::Rate rate(5);
-    stopString.data="stop";
 
     while(ros::ok()) {
         ros::spinOnce();
